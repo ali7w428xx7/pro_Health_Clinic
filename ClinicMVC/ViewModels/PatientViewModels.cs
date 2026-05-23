70d684e40542ea1503881a6d7aaa8948741a1d6b
Cyph3r-6 <rawhhasan@gmail.com>
2026-05-23 07:44:07 +0300

    Add ViewModels and data annotations for validation

---
+++ ClinicMVC/ViewModels/PatientViewModels.cs
@@ -0,0 +1,47 @@
+namespace ClinicMVC.ViewModels;
+
+public class PatientProfileViewModel
+{
+    public int Id { get; set; }
+    public string FullName { get; set; } = "";
+    public string Email { get; set; } = "";
+    public string CPRNumber { get; set; } = "";
+    public string PatientReferenceNumber { get; set; } = "";
+    public DateTime DateOfBirth { get; set; }
+    public string Gender { get; set; } = "";
+    public string Phone { get; set; } = "";
+    public string Address { get; set; } = "";
+    public string? BloodType { get; set; }
+    public string? Allergies { get; set; }
+    public List<AppointmentRowViewModel> RecentAppointments { get; set; } = [];
+    public List<VisitHistoryViewModel> VisitHistory { get; set; } = [];
+}
+
+public class VisitHistoryViewModel
+{
+    public int AppointmentId { get; set; }
+    public DateOnly AppointmentDate { get; set; }
+    public string DoctorName { get; set; } = "";
+    public string SpecializationName { get; set; } = "";
+    public string Diagnosis { get; set; } = "";
+    public string TreatmentPlan { get; set; } = "";
+    public string DoctorNotes { get; set; } = "";
+    public List<PrescriptionViewModel> Prescriptions { get; set; } = [];
+}
+
+public class PatientListViewModel
+{
+    public List<PatientSummaryViewModel> Patients { get; set; } = [];
+    public string? Search { get; set; }
+}
+
+public class PatientSummaryViewModel
+{
+    public int Id { get; set; }
+    public string FullName { get; set; } = "";
+    public string CPRNumber { get; set; } = "";
+    public string Phone { get; set; } = "";
+    public string Gender { get; set; } = "";
+    public int AppointmentCount { get; set; }
+    public DateOnly? LastVisit { get; set; }
+}
